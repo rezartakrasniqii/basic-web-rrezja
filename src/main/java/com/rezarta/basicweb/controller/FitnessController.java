bd534406c5076cefa673e33bfa0616a4d1efe65c
RezartaK <rezarta.krasniqi@student.cacttus.education>
2021-11-08 17:53:15 +0100

    updated

--- src/main/java/com/rezarta/basicweb/controller/FitnessController.java
+++ src/main/java/com/rezarta/basicweb/controller/FitnessController.java
@@ -8,8 +8,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.ArrayList;
+
 @Controller
 public class FitnessController {
+
+    ArrayList <FitnessUser> fitnessUsers = new ArrayList<>();
+
     @GetMapping("/fitnessapp")
     public String getFitness(Model model) {
         model.addAttribute("fitnessUser", new FitnessUser());
@@ -21,5 +26,8 @@ public class FitnessController {
         model.addAttribute("fitnessUser", fitnessUser);
         return "ViewFitnessUsers";
     }
-
+@GetMapping (/"ViewFitnessUsers")
+    public String getViewUsers () {
+        return "ViewFitnessUsers";
+}
 }
